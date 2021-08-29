3cbf5c2e36cb4f755f3e90dd2973743cf382f8b6
IdanHerman12 <idanh156@gmail.com>
2021-08-29 16:35:39 +0300

    testing the code

--- excercise 2.js
+++ excercise 2.js
@@ -1,10 +1,10 @@
 'use strict';
 const a = 1;
-const b = -6;
-const c = 9;  // stands for x**2 - 6*x + 9 = 0
+const b = 5;
+const c = 6;  // stands for x**2 - 6*x + 9 = 0
 let discriminant=b * b - 4 * a * c;
 let solutionA = 0;
 let solutionB = 0;
-solutionA+= (-b +Math.sqrt(discriminant))/(2*a);
-solutionB+=-((-b +Math.sqrt(discriminant))/(2*a));
+solutionA+=(-b + Math.sqrt(discriminant))/(2*a);
+solutionB+=(-b - Math.sqrt(discriminant))/(2*a);
 console.log(solutionA, solutionB);
