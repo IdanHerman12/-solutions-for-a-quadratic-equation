f427cb0f04c2b0c7998cc4dbaa215a38d62d7029
IdanHerman12 <idanh156@gmail.com>
2021-08-29 16:32:16 +0300

    setting the formula

---
+++ excercise 2.js
@@ -0,0 +1,10 @@
+'use strict';
+const a = 1;
+const b = -6;
+const c = 9;  // stands for x**2 - 6*x + 9 = 0
+let discriminant=b * b - 4 * a * c;
+let solutionA = 0;
+let solutionB = 0;
+solutionA+= (-b +Math.sqrt(discriminant))/(2*a);
+solutionB+=-((-b +Math.sqrt(discriminant))/(2*a));
+console.log(solutionA, solutionB);
